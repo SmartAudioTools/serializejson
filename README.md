de0d13ff78be61c9b0b90103572b12a8f11d461a
Baptiste de La Gorce <baptiste.delagorce@smartaudiotools.com>
2020-10-16 16:54:41 +0200

    change documentation

--- README.md
+++ README.md
@@ -10,17 +10,21 @@ Do not load serializejsons from untrusted / unauthenticated sources without carf
 - serialize arbitrary python objects (bytes and bytearray are very quikly serialized and deserializaed in base64).
 - call the sames objects methodes than pickle. therefore allmost all pickable objects are serializable with serializejson without any modification.
 - serialized objects are human-readable. (Your data will never be unreadable if your code evolved, you will allway be able to modify your data, unlike with pickle)
-- serialized objects take generaly less space than with pickle, a juste a little 30% more if big binaries data (numpy array, bytes, bytearray)
+- serialized objects take generaly less space than with pickle and juste a little 30% more if big binaries data (numpy array, bytes, bytearray)
 - only two time slower than pickle and much more faster than jsonpickle.
 - can safely load untrusted / unauthenticated sources if autorized_classes list parameter is set carfuly with stricly necessary objects (unlike pickle). 
 - can update an existings objects instead of overide thems (serializejson can be used to save and restore in place a complet application state).
 - filter attribut starting with "_" by default (unlike pickle).
 - numpy arrays can be serialized in list with automatique conversion in both way or in a conservative way. 
-- support circular references and serialize only once time duplicated object (if in a object attribut, not yet list or dictionnaries).
+- support circular references and serialize only once duplicated object (WARNING :not yet if the object is a list or dictionnary).
 - try to call attributs setters and propreties setters when loading if set_attributs  = True.
 - accept json with comment (// and /* */).
 - can automaticly recognize objects in json and recreate them, without the need of "`__class__`" key, if passeds in recognized_classes.
 - dump and load support string path. 
+- can iterativly encode (with append) and decode (with iterator) a list in json, saving memmory space during the process of serialization et deserialization.
+- WARNING : tuple, time.struct_time, collections.Counter, collections.OrderedDict, collections.defaultdict, namedtuples and dataclass are not yet correctly serialized 
+
+
 
 # Install
 pip install git+https://github.com/SmartAudioTools/serializejson.git
--- SmartFramework/__init__.py
+++ SmartFramework/__init__.py
@@ -27,3 +27,63 @@ import designer
 import midi
 #------------------
 '''
+from pybase64 import b64decode
+from numpy import frombuffer,unpackbits,uint8,ndarray,int32,int64
+from numpy import dtype as numpy_dtype
+from SmartFramework.serialize import serializeParameters
+import sys
+
+#defaultIntType =  numpy_dtype("int_")
+nb_bits = sys.maxsize.bit_length()+1
+
+def bytearrayB64(b64):
+    return bytearray(b64decode(b64,validate=True))
+
+def bytesB64(b64):
+    return b64decode(b64,validate=True) 
+            
+def numpyB64(str64,dtype = None,shapeOrLen = None):
+    decodedBytes = b64decode(str64,validate=True)    # str64 -> bytes : decodage avec copie 
+    if dtype in ("bool",bool) : 
+        uintContaining8bits = frombuffer(decodedBytes,uint8)    # pas de copie ?
+        uintContaining1bit  = unpackbits(uintContaining8bits) # copie
+        if shapeOrLen is None : 
+            return uintContaining1bit 
+        else : 
+            return ndarray(shapeOrLen,dtype,uintContaining1bit)      # a priori pas de recopie
+    else : 
+        if isinstance(dtype,list):
+            dtype = [(str(champName),champType) for champName,champType in dtype]
+        if shapeOrLen is None :
+            #return nympy.frombuffer(decodedBytes)
+            array = frombuffer(decodedBytes,dtype) # evite de faire une recopie 
+        else :
+            array = ndarray(shapeOrLen,dtype,decodedBytes) # evite de faire une recopie  ? a priori oui
+        array.flags.writeable = True
+        
+        if nb_bits == 32 and serializeParameters.numpyB64_convert_int64_to_int32_and_align_in_Python_32Bit : # pour pouvoir deserialiser les classifiers en python 32 bit ?
+            if array.dtype in (int64, 'int64'):
+                array = array.astype(int32)
+            elif isinstance(dtype,list):
+                newTypes = []
+                for champ  in dtype :
+                    champName,champType = champ
+                    if champName  : 
+                        champType = numpy_dtype(champType)
+                        if champType in (int64, 'int64'):
+                            newTypes.append((champName,int32))
+                        else : 
+                            newTypes.append((champName,champType)) 
+                newDtype = numpy_dtype(newTypes,align = True)
+                newN =  ndarray(len(array),newDtype) 
+                for champName,champType in newTypes : 
+                    if champName:
+                        newN[champName][:] = array[champName]
+                array = newN
+        return array   
+            #return numpy.ndarray(shape,dtype,bytearray(decodedBytes)) # obligé de mettre bytearray? sinon le tableau sera immutable , et si on force à l'etre avec flags.readonly = False on va changer la chaine de caractère sous jacent et potientiellement foutre la merde sur dictionnaire qui ont la meme clef?
+        
+    # str -> bytearray : copie ? 
+    # bytearray -> nympy : pas de copie 
+    #a.flags.writable = True # a priori dangereux , car va pouvoir modifier valeur d'une string... qui est sencé etre imutable .
+fromB64 = numpyB64 # pour pouvoir ouvrir d'anciens fichiers serializés
