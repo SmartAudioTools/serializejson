50bfc923647e5f7e69034d38da36f58f4e2f0258
Baptiste de La Gorce <baptiste.delagorce@smartaudiotools.com>
2020-10-19 21:47:46 +0200

    create_venv_baptiste.bat : allow creation on venv on baptiste computeur

---
+++ create_venv_baptiste.bat
@@ -0,0 +1,3 @@
+D:/Projets/Python/SmartPython/SmartPython-3.7.1.0-64bit/python-3.7.1.amd64/python.exe -m venv python_venv
+.\python_venv\scripts\pip.exe install -r requirements.txt
+.\python_venv\scripts\pip.exe install -e .
