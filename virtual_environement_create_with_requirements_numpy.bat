b7aad3d6cf8ef33cd954bfe5e5349d6a31bb9324
Baptiste de La Gorce <baptiste.delagorce@smartaudiotools.com>
2020-10-20 19:46:28 +0200

    permet creation d'un environement virtuel avec numpy

---
+++ virtual_environement_create_with_requirements_numpy.bat
@@ -0,0 +1,9 @@
+rmdir .\python_venv\
+IF EXIST "D:/Projets/Python/SmartPython/SmartPython-3.7.1.0-64bit/python-3.7.1.amd64/python.exe" (
+  D:/Projets/Python/SmartPython/SmartPython-3.7.1.0-64bit/python-3.7.1.amd64/python.exe -m venv python_venv
+) ELSE (
+  python -m venv python_venv
+)
+.\python_venv\scripts\pip.exe install -r requirements_numpy.txt
+.\python_venv\scripts\pip.exe install -e .
+%~dp0/python_venv/Scripts/activate
