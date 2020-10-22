e732d05d1d9bd8fef890f881f71203fc5074c5fa
Baptiste de La Gorce <baptiste.delagorce@smartaudiotools.com>
2020-10-22 12:11:54 +0200

    scipts pour pousser sur test.pypi.org

--- scripts/push_on_test_pipy_org.bat
+++ scripts/push_on_test_pipy_org.bat
@@ -1,5 +1,7 @@
 cd ..
+.\python_venv\scripts\pip.exe install wheel
+.\python_venv\scripts\pip.exe install twine
 .\python_venv\scripts\python.exe setup.py sdist
-.\python_venv\scripts\activate
 .\python_venv\scripts\python.exe setup.py bdist_wheel
-.\python_venv\scripts\python.exe twine upload --repository-url https://test.pypi.org/legacy/ dist/*   
+.\python_venv\scripts\python.exe .\python_venv\Lib\site-packages\twine\__main__.py upload --repository-url https://test.pypi.org/legacy/ dist/*   
+cd scripts
