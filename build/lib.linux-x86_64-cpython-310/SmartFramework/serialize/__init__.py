fe187ea5a2e921807d7abfd0b94655f882da8689
smartaudiotools <smartaudiotools@DELL3551.lan>
2023-06-11 22:53:32 +0200

    tente debugage github

--- build/lib.linux-x86_64-cpython-310/SmartFramework/serialize/__init__.py
+++
@@ -1,7 +0,0 @@
-﻿class ExceptionPyQt(Exception):  # exception pour tout le module
-    pass
-
-
-# from SmartFramework.tools.dictionaries import sorted_filtered
-
-# __all__  = ['serializejson','serializePython','serializeAsModule','serializeTiny','serializeRepr','serialize_parameters','ExceptionPyQt']
